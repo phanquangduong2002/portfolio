471b95de1bfb65f604e5aa0aeffbd561089c6135
phanquangduong2002 <phanquangduong2002@gmail.com>
2022-10-05 19:40:52 +0700

    .

--- src/App.js
+++ src/App.js
@@ -15,22 +15,16 @@ import {
 function App() {
     return (
         <>
-            {window.screen.width > 1024 ? (
-                <div className="w-full h-auto flex flex-col justify-center relative">
-                    <CustomCursor />
-                    <Header />
-                    <HomeContainer />
-                    <Process />
-                    <Portfolio />
-                    <About />
-                    <Contact />
-                    <ClickToTop />
-                </div>
-            ) : (
-                <div className=" w-screen h-screen flex items-center justify-center text-center bg-black text-white text-base tracking-wide font-light">
-                    Please view this demo on a desktop to see the effect.
-                </div>
-            )}
+            <div className="w-full h-auto flex flex-col justify-center relative">
+                <CustomCursor />
+                <Header />
+                <HomeContainer />
+                <Process />
+                <Portfolio />
+                <About />
+                <Contact />
+                <ClickToTop />
+            </div>
         </>
     );
 }
